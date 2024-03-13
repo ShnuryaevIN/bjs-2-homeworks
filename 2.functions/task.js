7dc4aad49c7925d13cad731b76dc1d35867c30f8
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-03-13 23:28:34 +0500

    Задание 1: Перебор массива выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,30 @@
-function getArrayParams(...arr) {
+"use strict"
 
-  return { min: min, max: max, avg: avg };
+let getArrayParams = (...nums) => {
+	let min = Math.min(...nums);
+	let max = Math.max(...nums);
+	let sum = 0;
+	let avg;
+
+	for (let elem of nums) {
+		if (elem > max) {
+			max = elem;
+		} else if (elem < min) {
+			min = elem;
+		}
+		sum += elem / nums.length;
+		avg = +sum.toFixed(2);
+	}
+	return {
+		min,
+		max,
+		avg,
+	};
 }
+
+console.log(getArrayParams(5));
+console.log(getArrayParams(-90, 90, 10));
+console.log(getArrayParams(1, 2, 3, -100, 10));
 
 function summElementsWorker(...arr) {
 
