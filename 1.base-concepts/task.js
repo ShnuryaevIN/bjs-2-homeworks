8f903e7bd99e59bb0144c1ece8220e48cb70889a
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-03-13 23:57:51 +0500

    Задание 1: Основные понятия выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,31 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - (4 * a * c);
+	if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+
+	return arr;
 }
+
+solveEquation(10, 5, 5);
+
+
+
+
+
+
+
+
+
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
