c446df5274697df5e13527beb3e297fdf7893f59
Shnuryaev I.N <shnuryaevin@gmail.com>
2024-03-14 23:35:46 +0500

    Задание 2: Функции выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -26,21 +26,77 @@ console.log(getArrayParams(5));
 console.log(getArrayParams(-90, 90, 10));
 console.log(getArrayParams(1, 2, 3, -100, 10));
 
-function summElementsWorker(...arr) {
 
+
+
+function summElementsWorker(...arr) {
+	let summ = 0;
+
+	for (let elem of arr) {
+			summ += elem;
+		} 
+	return summ;
 }
 
 function differenceMaxMinWorker(...arr) {
+	let min = Math.min(...arr);
+	let max = Math.max(...arr);
+  
+	if( !Array.isArray(arr) || !arr.length) {
+	  return 0;
+	} else {
+	  return max - min;
+	}
+  }
 
-}
+  function differenceEvenOddWorker(...arr) {
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
 
-function differenceEvenOddWorker(...arr) {
-
+	for (let elem of arr) {
+		if (elem % 2 == 0) {
+			sumEvenElement += elem;
+		} else if (elem % 2 != 0) {
+			sumOddElement += elem;
+		}
+	}
+	if (!Array.isArray(arr) || !arr.length) {
+		return 0;
+	} else {
+		return sumEvenElement - sumOddElement;
+	}
 }
 
 function averageEvenElementsWorker(...arr) {
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
 
+	for (let elem of arr) {
+		if (elem % 2 == 0) {
+			sumEvenElement += elem;
+			countEvenElement++;
+		}
+	}
+
+	if  ( !Array.isArray(arr) || !arr.length) {
+		return 0;
+	} else {
+		return sumEvenElement / countEvenElement;
+	}
 }
+  
+
+console.log(summElementsWorker()); // 0
+console.log(summElementsWorker(10, 10, 11, 20, 10));
+
+console.log(differenceMaxMinWorker()); // 0
+console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
+
+console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17));
+console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
+
+console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); 
+console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); 
 
 function makeWork (arrOfArr, func) {
 
